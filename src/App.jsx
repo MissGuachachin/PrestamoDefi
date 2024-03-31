0214a7dddd21ae2e982987ffb61c873d79b5f40d
Maggie <guachachi02@yahoo.es>
2024-03-31 20:24:27 +0200

    Actualizar componente App con AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { AppLayout } from './components/ui/layouts'
+
 function App() {
   
-  return <h1 className="bg-red-200 text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <AppLayout>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </AppLayout>
+  )
 }
 
 export default App
