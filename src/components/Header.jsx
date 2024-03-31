90010725cb068455122aa2888a713659520cd6eb
Maggie <guachachi02@yahoo.es>
2024-03-31 20:20:11 +0200

    Añadir componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="bg-black border-b py-20 px-20">
+            <img src="/EthDefiLogo.jpeg" alt="defi-logo" width={600} />
+        </header>
+    );
+}
