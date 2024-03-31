3a94f84bf76c9d510f68c4980f7466fb3bfd3ca2
Maggie <guachachi02@yahoo.es>
2024-03-31 23:10:15 +0200

    Actualizar componente App añadiendo configuracion wagmi y connectkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { ConnectKitProvider, ConnectKitButton } from "connectkit"
+import { WagmiConfig } from "wagmi"
 import { AppLayout } from './components/ui/layouts'
+import { config } from './config/wagmi'
 import { HomePage } from './pages'
 
 function App() {
   
   return (
-    <AppLayout>
-      <HomePage />
-    </AppLayout>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <AppLayout>
+          <HomePage />
+          <ConnectKitButton showBalance/>
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   )
 }
 
