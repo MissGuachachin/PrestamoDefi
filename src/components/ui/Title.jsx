823c95bc022d661ede839606bc48b035f2a9f77d
Maggie <guachachi02@yahoo.es>
2024-04-02 20:25:51 +0200

    Actualizar componente ui Title

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 export default function Title({ children }) {
-    return <h3 className='text-5xl font-bold text-blue-700 py-10 flex justify-center'>{children}</h3>
+    return <h3 className='text-5xl font-bold text-indigo-500 py-10 flex justify-center'>{children}</h3>
     
 }
 
