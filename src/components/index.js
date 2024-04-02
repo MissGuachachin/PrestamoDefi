8d47613ae3c11573dcf22bbbac4d9e44f1bf080d
Maggie <guachachi02@yahoo.es>
2024-04-02 20:30:17 +0200

    Añadir componentes en indice

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
+export { default as AltaPrestamista } from './AltaPrestamista';
+export { default as AltaCliente } from './AltaCliente';
