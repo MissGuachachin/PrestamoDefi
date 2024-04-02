9094a1a99fe7660d721ac7f86bc8c10e86babfa2
Maggie <guachachi02@yahoo.es>
2024-04-03 00:52:30 +0200

    Actualizar indice eliminado elementos pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as HomePage } from './HomePage';
 export { default as GestionDeUsuarios } from './GestionDeUsuarios'
 export { default as Alert } from './Alert';
-export { default as NotFound } from './NotFound';
+export { default as NotFoundPage } from './NotFoundPage';
