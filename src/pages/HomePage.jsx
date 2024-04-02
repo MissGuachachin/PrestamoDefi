f3dd4cf29dfd12cdc55cbe1486252a57f7ca753c
Maggie <guachachi02@yahoo.es>
2024-04-02 21:24:57 +0200

    Actualizar pages HomePages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,17 @@
 import { useAccount } from "wagmi"
-import { useState } from "react";
-import { Title, LoadingSpinner} from "../components/ui"
-import { AltaPrestamista, AltaCliente } from "../components"
+import { LoadingSpinner} from "../components/ui"
+import { GestionDeUsuarios } from "../pages"
 
 
 export default function HomePage() {
   const { isConnected} = useAccount()
-  const [nuevoPrestamista, setNuevoPrestamista] = useState(''); // Agrega useState para el nuevo prestamista
-  const [nuevoCliente, setNuevoCliente] = useState('');
-
-  const handleAltaPrestamista = (nuevoPrestamista) => { // Define handleAltaPrestamista para pasar al componente AltaPrestamista
-    // Aquí debes implementar la lógica para dar de alta al prestamista
-    console.log("Prestamista dado de alta:", nuevoPrestamista);
-    setNuevoPrestamista('');
-  };
   
-  const handleAltaCliente = (nuevoCliente) => {
-    console.log("Cliente dado de alta:", nuevoCliente);
-    setNuevoCliente('');
-  };
 
   return (
       <div className="flex flex-col self-center sm:grid place-items-center px-40 py-16 md-:px-5 gap-y-20">
         {isConnected ? (
         <>
-          <section className='bg-white border-4 border-indigo-300 shadow p-4 px-20 rounded-3xl w-[780px]'>
-            <Title>Gestión de Usuarios</Title>
-          </section>
-          <section className='flex justify justify-between gap-x-10'>
-            <div className='flex justify-between bg-white border-2 border-gray-300 shadow p-2 py-5 px-5 rounded-3xl'>
-              <AltaPrestamista altaPrestamista={handleAltaPrestamista} nuevoPrestamista={nuevoPrestamista} setNuevoPrestamista={setNuevoPrestamista}/>
-            </div>
-            <div className='flex justify-between bg-white border-2 border-gray-300 shadow p-2 py-5 px-5 rounded-3xl'>
-            <AltaCliente altaCliente={handleAltaCliente} nuevoCliente={nuevoCliente}/>
-            </div>
-          </section>
+          <GestionDeUsuarios />
         </>
         ) : (
           <>
@@ -43,7 +20,6 @@ export default function HomePage() {
             </h1>
             <p className="text-3xl sm:text-3xl py-3">🔒 Conecta a tu Wallet...</p>
             <LoadingSpinner />
-          
           </>
        )}
       </div>
