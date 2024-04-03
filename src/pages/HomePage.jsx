74581db5668543b873a624003b01b1ce7377276f
Maggie <guachachi02@yahoo.es>
2024-04-03 21:14:13 +0200

    Añadir componentes a HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,30 @@
-import { useAccount } from "wagmi"
-import { Button, LoadingSpinner } from "../components/ui"
-
+import { useAccount, } from "wagmi"
+import { LoadingSpinner } from "../components/ui"
+import { GestionDeUsuarios, OperacionesPrestamosPage } from "./index"
 
 export default function HomePage() {
   const { isConnected } = useAccount()
-
+  
   return (
-      <div className="flex flex-col self-center sm:grid place-items-center px-40 py-16 md-:px-5 gap-y-20">
+      <div className="flex flex-col self-center sm:grid place-items-center px-40 py-16 md-:px-5 gap-20">
         {isConnected ? (
         <>
-          <section className='flex flex-col self-center py-20 px-20 gap-12'>
-            <div className='p-10 border shadow rounded-lg text-lg w-[690px] sm:w-[720px]'>
-              <Button>Gestión de Usuarios</Button>
+          <section className='flex flex-col xl:flex-row py-6 px-10 gap-20'>
+            <div className='p-6 bg-blue-200 border shadow rounded-lg w-[500px] sm:w-[700px]'>
+              {/* Renderizar el componente GestionDeUsuarios */}
+              <GestionDeUsuarios>Gestión de Usuarios</GestionDeUsuarios>
             </div>
-            <div className='p-10 border shadow rounded-lg text-lg w-[690px] sm:w-[720px]'>
-              <Button>OperacionesPrestamosPage</Button>
+            <div className='p-6 bg-blue-200 border shadow rounded-lg w-[500px] sm:w-[700px]'>
+              {/* Renderizar el componente OperacionesPrestamosPage */}
+              <OperacionesPrestamosPage />
             </div>
           </section>
         </>
         ) : (
           <>
-            <h1 className='text-6xl font-bold text-blue-700 py-10 flex justify-center'>
-            Préstamos de Finanzas Descentralizadas (Defi)
+            <h1 className='text-6xl font-bold sm:text-5xl md:text-6xl text-blue-700 py-10
+              flex justify-center text-transparente bg-clip-text'>
+              Préstamos de Finanzas Descentralizadas (Defi)
             </h1>
             <p className="text-3xl sm:text-3xl py-3">🔒 Conecta tu Wallet para comenzar...</p>
             <LoadingSpinner />
