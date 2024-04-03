6f4ba29fd92416b57f3f4191fe8fed3838a2af99
Maggie <guachachi02@yahoo.es>
2024-04-03 21:12:35 +0200

    Crear y añadir componente OperacionesPrestamosPage a HomePage

--- src/pages/OperacionesPrestamosPage.jsx
+++ src/pages/OperacionesPrestamosPage.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react';
-import { DepositarGarantia, SolicitarPrestamo, AprobarPrestamo, ReembolsarPrestamo, LiquidarGarantia, PrestamosCliente } from '../components'
+import { Title } from "../components/ui"
+import { 
+  DepositarGarantia,
+  SolicitarPrestamo,
+  AprobarPrestamo,
+  ReembolsarPrestamo,
+  LiquidarGarantia,
+  PrestamosCliente
+} from '../components'
+
 
 const OperacionesPrestamosPage = () => {
   const [prestatario, setPrestatario] = useState(''); 
@@ -9,29 +18,32 @@ const OperacionesPrestamosPage = () => {
   };
 
   return (
-    <div>
-      
-      <div>
-        <h3>Seleccionar Prestatario:</h3>
-        <select onChange={(e) => handleSelectPrestatario(e.target.value)}>
-          <option value="">Seleccionar</option>
-          {/* Aquí puedes generar dinámicamente opciones para todos los prestatarios */}
-          {/* Ejemplo: <option value="direccion_prestatario">Nombre del Prestatario</option> */}
-        </select>
-      </div>
-      {prestatario && (
+    <section>
+      <Title>Operaciones para Préstamo{prestatario}:</Title>
         <div>
-          <h3>Operaciones para Préstamo{prestatario}:</h3>
-          <DepositarGarantia prestatario={prestatario} />
-          <SolicitarPrestamo prestatario={prestatario} />
-          <AprobarPrestamo prestatario={prestatario} />
-          <ReembolsarPrestamo prestatario={prestatario} />
-          <LiquidarGarantia prestatario={prestatario} />
-          <PrestamosCliente prestatario={prestatario} />
+          <div className='px-3'>
+            <h3 className='font-sans text-2xl items-stretch font-bold text-indigo-600 py-3 flex flex-col'>
+              Seleccionar Prestatario:
+            </h3>
+              <select className='py-2 px-20 rounded-lg text-gray-400' onChange={(e) => handleSelectPrestatario(e.target.value)}>
+              <option>Seleccionar</option>
+              {/* Aquí puedes generar dinámicamente opciones para todos los prestatarios */}
+              {/* Ejemplo: <option value="direccion_prestatario">Nombre del Prestatario</option> */}
+              </select>
+          </div>
+              {prestatario && (
+          <div>
+            <DepositarGarantia prestatario={prestatario} />
+            <SolicitarPrestamo prestatario={prestatario} />
+            <AprobarPrestamo prestatario={prestatario} />
+            <ReembolsarPrestamo prestatario={prestatario} />
+            <LiquidarGarantia prestatario={prestatario} />
+            <PrestamosCliente prestatario={prestatario} />
+          </div>
+              )}
         </div>
-      )}
-    </div>
+    </section>
   );
 };
 
-export default OperacionesPrestamosPage;
+export default OperacionesPrestamosPage
