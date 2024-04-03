cb4423236e748b7ea7412491d55053fa33ad6be6
Maggie <guachachi02@yahoo.es>
2024-04-03 12:54:00 +0200

    Actualizar y eliminar componentes en pages indice

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,1 @@
 export { default as HomePage } from './HomePage';
-export { default as GestionDeUsuarios } from './GestionDeUsuarios'
-export { default as Alert } from './Alert';
-export { default as NotFoundPage } from './NotFoundPage';
