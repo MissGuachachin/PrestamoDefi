7fda69e9115b56e94efa3eb599ea4b5bada36ecf
Maggie <guachachi02@yahoo.es>
2024-04-03 12:58:24 +0200

    Actualizar HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,32 @@
 import { useAccount } from "wagmi"
-import { LoadingSpinner} from "../components/ui"
-import { GestionDeUsuarios } from "../pages"
+import { Button, LoadingSpinner } from "../components/ui"
 
 
 export default function HomePage() {
-  const { isConnected} = useAccount()
-  
+  const { isConnected } = useAccount()
 
   return (
       <div className="flex flex-col self-center sm:grid place-items-center px-40 py-16 md-:px-5 gap-y-20">
         {isConnected ? (
         <>
-          <GestionDeUsuarios />
+          <section className='flex flex-col self-center py-20 px-20 gap-12'>
+            <div className='p-10 border shadow rounded-lg text-lg w-[690px] sm:w-[720px]'>
+              <Button>Gestión de Usuarios</Button>
+            </div>
+            <div className='p-10 border shadow rounded-lg text-lg w-[690px] sm:w-[720px]'>
+              <Button>OperacionesPrestamosPage</Button>
+            </div>
+          </section>
         </>
         ) : (
           <>
             <h1 className='text-6xl font-bold text-blue-700 py-10 flex justify-center'>
             Préstamos de Finanzas Descentralizadas (Defi)
             </h1>
-            <p className="text-3xl sm:text-3xl py-3">🔒 Conecta a tu Wallet...</p>
+            <p className="text-3xl sm:text-3xl py-3">🔒 Conecta tu Wallet para comenzar...</p>
             <LoadingSpinner />
           </>
        )}
       </div>
-       
   )
 }
