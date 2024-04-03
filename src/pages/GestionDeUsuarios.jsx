a53cd35ee82b7e6292ec41a928e19502edeed875
Maggie <guachachi02@yahoo.es>
2024-04-03 21:11:05 +0200

    Actualizar cambios GestionDeUsuarios

--- src/pages/GestionDeUsuarios.jsx
+++ src/pages/GestionDeUsuarios.jsx
@@ -3,11 +3,10 @@ import { AltaPrestamista, AltaCliente } from "../components"
 import { Title } from "../components/ui"
 
 export default function GestionDeUsuarios() {
-    const [nuevoPrestamista, setNuevoPrestamista] = useState(''); // Agrega useState para el nuevo prestamista
+  const [nuevoPrestamista, setNuevoPrestamista] = useState('');
   const [nuevoCliente, setNuevoCliente] = useState('');
 
-  const handleAltaPrestamista = (nuevoPrestamista) => { // Define handleAltaPrestamista para pasar al componente AltaPrestamista
-    // Aquí debes implementar la lógica para dar de alta al prestamista
+  const handleAltaPrestamista = (nuevoPrestamista) => { 
     console.log("Prestamista dado de alta:", nuevoPrestamista);
     setNuevoPrestamista('');
   };
@@ -19,14 +18,14 @@ export default function GestionDeUsuarios() {
     
     return (
         <>
-        <section className='bg-white border-4 border-indigo-300 shadow p-4 px-20 rounded-3xl w-[780px]'>
+        <section>
             <Title>Gestión de Usuarios</Title>
           </section>
-          <section className='flex justify justify-between gap-x-10'>
-            <div className='flex justify-between bg-white border-2 border-gray-300 shadow p-2 py-5 px-5 rounded-3xl'>
+          <section>
+            <div>
               <AltaPrestamista altaPrestamista={handleAltaPrestamista} nuevoPrestamista={nuevoPrestamista} setNuevoPrestamista={setNuevoPrestamista}/>
             </div>
-            <div className='flex justify-between bg-white border-2 border-gray-300 shadow p-2 py-5 px-5 rounded-3xl'>
+            <div>
             <AltaCliente altaCliente={handleAltaCliente} nuevoCliente={nuevoCliente}/>
             </div>
           </section>
