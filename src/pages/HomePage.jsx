8d22b87b519f38c90aa6cb59f055008db84bafb0
Maggie <guachachi02@yahoo.es>
2024-04-05 19:59:33 +0200

    Actualizar componente HomePage de pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,14 @@ import { useAccount, } from "wagmi"
 import { LoadingSpinner } from "../components/ui"
 import { GestionDeUsuarios, OperacionesPrestamosPage } from "./index"
 
+
 export default function HomePage() {
   const { isConnected } = useAccount()
   
   return (
       <div className="flex flex-col self-center sm:grid place-items-center px-40 py-16 md-:px-5 gap-20">
         {isConnected ? (
-        <>
+        <>       
           <section className='flex flex-col xl:flex-row py-6 px-10 gap-20'>
             <div className='p-6 bg-blue-200 border shadow rounded-lg w-[500px] sm:w-[700px]'>
               {/* Renderizar el componente GestionDeUsuarios */}
